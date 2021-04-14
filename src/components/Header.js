a476533bfb0182a29168c4978c037bc5aa133857
Bartosz Bielecki <Bartosz.m.Bielecki@gmail.com>
2021-04-14 11:53:53 +0200

    complete Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./Header.sass";
+import wroclaw1 from "../img/wroclaw1.jpg";
+import wroclaw2 from "../img/wroclaw2.jpg";
+import wroclaw3 from "../img/wroclaw3.jpg";
 import { FaTemperatureHigh } from "react-icons/fa";
 import { FiSunset, FiSunrise } from "react-icons/fi";
 
@@ -8,8 +11,11 @@ class Header extends Component {
     temperature: "",
     sunrise: "",
     sunset: "",
+    activeElement: 0,
+    backgroundImage: wroclaw1,
   };
 
+  backgroundImage = [wroclaw1, wroclaw2, wroclaw3];
   API_key = "c4c8854fb0426dd04df7f7f0d3bab41c";
   API_cityName = "Wroclaw";
   API = `http://api.openweathermap.org/data/2.5/weather?q=${this.API_cityName}&appid=${this.API_key}&units=metric`;
@@ -35,26 +41,35 @@ class Header extends Component {
           sunset,
         });
       });
+    this.backgroundInterval = setInterval(() => {
+      let activeElement = this.state.activeElement;
+      activeElement++;
+      if (activeElement === this.backgroundImage.length) activeElement = 0;
+      const backgroundImage = this.backgroundImage[activeElement];
+      this.setState({
+        activeElement,
+        backgroundImage,
+      });
+    }, 5000);
   }
+
   render() {
     const { temperature, sunrise, sunset } = this.state;
 
     return (
       <header>
+        <img src={this.state.backgroundImage} alt="wroclaw" />
         <div className="header">
           <h2>welcome in wroclaw</h2>
           <aside className="cityInfo">
             <p>
-              <FaTemperatureHigh />
-              {temperature}
+              <FaTemperatureHigh /> {temperature}
             </p>
             <p>
-              <FiSunrise />
-              {sunrise}
+              <FiSunrise /> {sunrise}
             </p>
             <p>
-              <FiSunset />
-              {sunset}
+              <FiSunset /> {sunset}
             </p>
           </aside>
         </div>
